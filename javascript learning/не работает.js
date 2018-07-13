55a9cfe16c79653028358dce8804d333ff660051
Aleksei Chernitsyn <Aleksei_Chernistyn@epam.com>
2018-07-13 14:57:05 +0400

    started to make grid

--- javascript learning/не работает.js
+++ javascript learning/не работает.js
@@ -1,43 +1,53 @@
 
-var plan =    ["############################",
-                "#      #    #      o      ##",
-                "#                          #",
-                "#          #####           #",
-                "##         #   #    ##     #",
-                "###           ##     #     #",
-                "#           ###      #     #",
-                "#   ####                   #",
-                "#   ##       o             #",
-                "# o  #         o       ### #",
-                "#    #                     #",
-                "############################"];
+// var plan =    [ "############################",
+//                 "#      #    #      o      ##",
+//                 "#                          #",
+//                 "#          #####           #",
+//                 "##         #   #    ##     #",
+//                 "###           ##     #     #",
+//                 "#           ###      #     #",
+//                 "#   ####                   #",
+//                 "#   ##       o             #",
+//                 "# o  #         o       ### #",
+//                 "#    #                     #",
+//                 "############################"];
+var plan = ['####',
+            '#o #',
+            '#  #',
+            '####']
+
 function World(plan) {
-    this.parse(plan);
+    this.parseToGrid();
 }
 function Animal() {
+
 }
 function Wall() {}
 
 World.prototype.show = function() {
     console.log('plan: \n'  + plan);
 }
-World.prototype.parse = function(plan) {
-    // var output = '';
+World.prototype.parseToGrid = function() {
+    var output = [];
     plan.forEach(element => {
         element.split('').forEach(elementOfRow => {
             if (elementOfRow === '#') {
-                elementOfRow = new Wall();
+
+                output.push(new Wall());
             } else if (elementOfRow === "o") {
-                elementOfRow = new Animal();
+                output.push(new Animal());
             } else {
-                elementOfRow = null;
+                output.push(null);
             }
         });
     });
-// return plan;
+    //return(output);
+    this.grid = output;
+    // console.log(output);
 }
-
+// console.log()
 var myWorld = new World(plan);
+
 function Vector(x, y) {
   this.x = x;
   this.y = y;
@@ -49,3 +59,20 @@ Vector.prototype.plus = function(other) {
 // var v2 = new Vector(2,2);
 // console.log(v1.plus(v2));
 myWorld.show();
+console.log(myWorld.grid);
+console.log(JSON.stringify(myWorld));
+// function Grid(width, height) {
+//     this.space = new Array(width * height);
+//     this.width = width;
+//     this.height = height;
+//   }
+//   Grid.prototype.isInside = function(vector) {
+//     return vector.x >= 0 && vector.x < this.width &&
+//            vector.y >= 0 && vector.y < this.height;
+//   };
+//   Grid.prototype.get = function(vector) {
+//     return this.space[vector.x + this.width * vector.y];
+//   };
+//   Grid.prototype.set = function(vector, value) {
+//     this.space[vector.x + this.width * vector.y] = value;
+//   };

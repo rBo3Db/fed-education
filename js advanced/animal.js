775c7f8de2906fc01fa80c27ced7736a3595113a
Aleksei Chernitsyn <Aleksei_Chernistyn@epam.com>
2018-07-24 10:41:22 +0400

    esLintrc added

--- js advanced/animal.js
+++ js advanced/animal.js
@@ -2,7 +2,7 @@ function Animal() {
     this.direction = randomElement(Object.keys(directions));
 }
 Animal.prototype.act = function(view) {
-    if (view.look(this.direction) != " ")
-        this.direction = view.find(" ") || "s";
-    return {type: "move", direction: this.direction};
-}
+    if (view.look(this.direction) != ' ')
+        this.direction = view.find(' ') || 's';
+    return {type: 'move', direction: this.direction};
+};

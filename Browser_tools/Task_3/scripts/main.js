7b11a6cf634ff97df18d2d952f48abb6b760a169
Aleksei Chernitsyn <Aleksei_Chernistyn@epam.com>
2018-07-26 18:32:19 +0400

    рабочий мейн

--- Browser_tools/Task_3/scripts/main.js
+++ Browser_tools/Task_3/scripts/main.js
@@ -1,2 +1,13 @@
 const history = window.history;
 document.getElementById('historyPosition').innerHTML = history.length;
+
+document.getElementById('history').addEventListener('click', function(e) {
+    e.stopPropagation(); // останавливаем всплытие
+    if (e.target.innerText == 'previos') {
+        history.go(-1);
+    }
+    if (e.target.innerText == 'next') {
+        history.go(+1);
+    }
+    ; // получаем и записываем содержимое элемента вызвавшего событие
+  }, false);
